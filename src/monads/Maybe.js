e5ed038b648b2ca0e125c53ac30c4f9fd474a07e
ChimiDEV <Tim.Walter1996@web.de>
2020-09-03 00:20:40 +0200

    ⃣¼[200~⃼¨~ feat: start working on Maybe

--- src/monads/Maybe.js
+++ src/monads/Maybe.js
@@ -1,7 +1,64 @@
 /**
+ * This is the implementation of a Maybe monad. It either contains a value or {@link Types.Nothing|Nothing}.
+ * It is compliant with the {@link https://github.com/fantasyland/fantasy-land|Fantasy-Land} specification and conforms following types:
+ * - {@link https://github.com/fantasyland/fantasy-land#setoid|Setoid}
+ * - {@link https://github.com/fantasyland/fantasy-land#Filterable|Filterable}
+ * - {@link https://github.com/fantasyland/fantasy-land#Functor|Functor}
+ * - {@link https://github.com/fantasyland/fantasy-land#Apply|Apply}
+ * - {@link https://github.com/fantasyland/fantasy-land#Applicative|Applicative}
+ * - {@link https://github.com/fantasyland/fantasy-land#Chain|Chain}
+ *
  * @class Maybe
+ *
+ * @example
+ * import { Maybe } from '@chimidev/dreamic';
+ *
+ * Maybe.of(10);
+ * // Maybe(10)
  */
 
-class Maybe {}
+/**
+ * @template T Type of the `Maybe` value
+ */
+class Maybe {
+  /**
+   * Usually you wouldn't use the constructor directly. Instead refer to `Maybe.of` or `Maybe.empty`.
+   * @private
+   * @param {*} value
+   * @param {'Just'|'Nothing'} tag
+   */
+  constructor(value, tag) {
+    this.$value = value;
+    this.$tag = tag;
+  }
+
+  /**
+   *
+   * @memberof Maybe
+   * @function
+   * @name of
+   *
+   * @template T Type of the value
+   * @param {T} value
+   * @returns {Maybe<T>}
+   */
+  static of(value) {
+    return new Maybe(value, 'Just');
+  }
+
+  /**
+   *
+   * @memberof Maybe
+   * @function
+   * @name empty
+   *
+   * @template T Type of the value
+   * @param {T} value
+   * @returns {Maybe<Nothing>}
+   */
+  static empty() {
+    return new Maybe(undefined, 'Nothing');
+  }
+}
 
 export default Maybe;
---
+++ src/typedef.js
@@ -0,0 +1,9 @@
+/**
+ * @namespace Types
+ */
+
+/**
+ * Defines `Nothing`. Mainly used by {@link Maybe}
+ * @memberof Types
+ * @typedef {undefined} Nothing
+ */
